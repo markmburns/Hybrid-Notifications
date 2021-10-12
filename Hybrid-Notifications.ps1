7fcf4ebcee07e3173154ba02ff792ff6065ada2f
markmburns <88446677+markmburns@users.noreply.github.com>
2021-10-12 09:23:16 +0100

    Update Hybrid-Notifications.ps1

--- Hybrid-Notifications.ps1
+++ Hybrid-Notifications.ps1
@@ -11,7 +11,7 @@ Custom protocol to restart device === https://www.imab.dk/windows-10-toast-notif
 Replacement for UserESP === https://docs.microsoft.com/en-us/troubleshoot/mem/intune/understand-troubleshoot-esp
 Also recommend script to speed up AD Connect sync
 Win32 install cmd: powershell.exe -noprofile -executionpolicy bypass -file .\Hybrid-Notifications.ps1
-Win32 uninstall: cmd.exe /c del %ProgramData%\Microsoft\RenameComputer\Hybrid-Notifications.ps1.tag
+Win32 uninstall: cmd.exe /c del %ProgramData%\Dell\Hybrid-Notifications\Hybrid-Notifications.ps1.tag
 Win32 detection: %ProgramData%\Dell\Hybrid-Notifications\Hybrid-Notifications.ps1.tag
 #>
 #Parameters
